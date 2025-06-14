1762a7cce07f883f026108e6b297f32abfa5b029
keshav0100 <kbansal1_be22@thapar.edu>
2025-06-14 12:51:02 +0530

    Refactor ProjectList component to include category and tag filters with RadioGroup; update button hover styles in CSS.

--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -1,48 +1,102 @@
-import React from "react";  
-import { Card, CardContent } from '@/components/ui/card'
-import { MixerHorizontalIcon } from '@radix-ui/react-icons'
-import { Button } from '@/components/ui/button'
+import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { MixerHorizontalIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
-
+const tags = [
+  "All",
+  "ReactJs",
+  "NextJs",
+  "SpringBoot",
+  "NLP",
+  "MySQL",
+  "MongoDB",
+  "Angular",
+  "Python3",
+  "Flask",
+  "Django",
+];
 const ProjectList = () => {
+  const handleFilterChange = (section, value) => {
+    console.log("value", value, section);
+  };
   return (
     <>
-    <div className='relative px-5 lg:px-0 lg:flex gap-5 justify-center py-8'>
-        <section className='filterSection'>
-        <Card className='p-5 sticky top-10'>
-            <div className='flex justify-between lg:w-[20rem]'>
-                <p className='text-xl -tracking-wider'>Filters</p>
-                <Button variant="ghost" size="icon">
-                    <MixerHorizontalIcon/>
-                </Button>
+      <div className="relative px-5 lg:px-0 lg:flex gap-5 justify-center py-8">
+        <section className="filterSection">
+          <Card className="p-5 sticky top-10">
+            <div className="flex justify-between lg:w-[20rem]">
+              <p className="text-xl -tracking-wider">Filters</p>
+              <Button variant="ghost" size="icon">
+                <MixerHorizontalIcon />
+              </Button>
             </div>
             <CardContent className="mt-5">
-                <ScrollArea className="space-y-7 h-[70vh] ">
-                     <div>
-                         <h1 className="pb-3 text-gray-400 border-b">
-                            Category
-                        </h1>
-                        <div className="pt-5">
-                            <RadioGroup>
-                                 <div>
-                                    {/* <RadioGroupItem value="all" id="r1"/> */}
-                                    {/* <Label htmlFor="r1"> All</Label> */}
-                                </div> 
-                            </RadioGroup> 
-                        </div> 
-                    </div>  
-                </ScrollArea>
-            </CardContent>
-        </Card>
-        </section>
-        <section className='projectSection w-full lg:w-[48rem]'>
-        </section>
-    </div> 
-    </>
-  )
-}
+              <ScrollArea className="space-y-7 h-[70vh] ">
+                <div>
+                  <h1 className="pb-3 text-gray-400 border-b">Category</h1>
+                  <div className="pt-5">
+                    <RadioGroup
+                      className="space-y-1 pt-2"
+                      defaultValue="all"
+                      onValueChange={(value) =>
+                        handleFilterChange("category", value)
+                      }
+                    >
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="all" id="r1" />
+                        <Label htmlFor="r1"> All</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="ai" id="r2" />
+                        <Label htmlFor="r2"> Artifical Intelligence</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="fullstack" id="r3" />
+                        <Label htmlFor="r3"> Full Stack</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="frontend" id="r4" />
+                        <Label htmlFor="r4"> Frontend</Label>
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <RadioGroupItem value="backend" id="r5" />
+                        <Label htmlFor="r5"> Backend</Label>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                </div>
 
-export default ProjectList
+                <div className="pt-9">
+                  <h1 className="pb-3 text-gray-400 border-b">Tag</h1>
+                  <div className="pt-5">
+                    <RadioGroup
+                      className="space-y-2 pt-2"
+                      defaultValue="all"
+                      onValueChange={(value) =>
+                        handleFilterChange("tag", value)
+                      }
+                    >
+                      {tags.map((item) => (
+                        <div key={item} className="flex items-center gap-2">
+                          <RadioGroupItem value={item} id={`r-${item}`} />
+                          <Label htmlFor={`r-${item}`}>{item}</Label>
+                        </div>
+                      ))}
+                    </RadioGroup>
+                  </div>
+                </div>
+              </ScrollArea>
+            </CardContent>
+          </Card>
+        </section>
+        <section className="projectSection w-full lg:w-[48rem]"></section>
+      </div>
+    </>
+  );
+};
+
+export default ProjectList;
