05dc048bf1d8dced3e463406241ef74b821326ff
keshav0100 <kbansal1_be22@thapar.edu>
2025-06-14 12:20:29 +0530

    RadioGroup Implemented

--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -1,9 +1,11 @@
 import React from "react";  
-
 import { Card, CardContent } from '@/components/ui/card'
 import { MixerHorizontalIcon } from '@radix-ui/react-icons'
 import { Button } from '@/components/ui/button'
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
 
 const ProjectList = () => {
   return (
@@ -18,7 +20,21 @@ const ProjectList = () => {
                 </Button>
             </div>
             <CardContent className="mt-5">
-                <ScrollArea className></ScrollArea>
+                <ScrollArea className="space-y-7 h-[70vh] ">
+                     <div>
+                         <h1 className="pb-3 text-gray-400 border-b">
+                            Category
+                        </h1>
+                        <div className="pt-5">
+                            <RadioGroup>
+                                 <div>
+                                    {/* <RadioGroupItem value="all" id="r1"/> */}
+                                    {/* <Label htmlFor="r1"> All</Label> */}
+                                </div> 
+                            </RadioGroup> 
+                        </div> 
+                    </div>  
+                </ScrollArea>
             </CardContent>
         </Card>
         </section>
