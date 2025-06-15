adbbd5cbfcb1a8c28391aca3cc96ae87ed248d66
keshav0100 <kbansal1_be22@thapar.edu>
2025-06-15 18:20:35 +0530

    Refactor CommentCard component to enhance UI with Avatar and TrashIcon; implement layout adjustments for improved user interaction.

--- src/pages/IssueDetails/CommentCard.jsx
+++ src/pages/IssueDetails/CommentCard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import { Avatar,AvatarFallback } from "@/components/ui/avatar"
+import { TrashIcon } from "@radix-ui/react-icons"
+import { Button } from "@/components/ui/button"
 const CommentCard = () => {
   return (
-    <div>
-      CommentCard
+    <div className="flex justify-between">
+      <div className="flex items-center gap-4">
+<Avatar>
+    <AvatarFallback>K</AvatarFallback>
+</Avatar>
+<div className="space-y-1">
+    <p>Keshav Bansal</p>
+    <p>How much work is left</p>
+</div>
+
+      </div>
+      <Button className="rounded-full" variant="ghost" size="icon">
+        <TrashIcon/>
+
+      </Button>
     </div>
   )
 }
