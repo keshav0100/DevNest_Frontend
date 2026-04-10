261d02e5722981b9d5a1c73ac1c9939041edb3d6
Keshav Bansal <bansalkeshav1390@gmail.com>
2026-04-10 17:28:41 +0530

    fix: use centralized axios instance

--- src/Redux/Auth/Action.js
+++ src/Redux/Auth/Action.js
@@ -1,12 +1,10 @@
-import { API_BASE_URL } from "@/config/api";
 import { GET_USER_REQUEST, GET_USER_SUCCESS, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, REGISTER_REQUEST, REGISTER_SUCCESS } from "./ActionType"
-import axios from "axios";
-
+import api from "@/config/api";
 
 export const register=userData=>async(dispatch)=>{
     dispatch({type:REGISTER_REQUEST})
     try {
-        const {data}=await axios.post(`${API_BASE_URL}/auth/signup`,userData);
+        const {data}=await api.post("/auth/signup",userData);
         if(data.jwt)
         {
             localStorage.setItem("jwt",data.jwt)
@@ -22,7 +20,7 @@ export const register=userData=>async(dispatch)=>{
 export const login=userData=>async(dispatch)=>{
     dispatch({type:LOGIN_REQUEST})
     try {
-        const {data}=await axios.post(`${API_BASE_URL}/auth/signing`,userData);
+        const {data}=await api.post("/auth/signing",userData);
         if(data.jwt)
         {
             localStorage.setItem("jwt",data.jwt)
@@ -37,7 +35,7 @@ export const login=userData=>async(dispatch)=>{
 export const getUser=()=>async(dispatch)=>{
     dispatch({type:GET_USER_REQUEST})
     try {
-        const {data}=await axios.get(`${API_BASE_URL}/api/users/profile`,{
+        const {data}=await api.get("/api/users/profile",{
             headers:{
                 "Authorization":`Bearer ${localStorage.getItem("jwt")}`
             },
